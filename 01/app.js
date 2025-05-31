f058306ff9ea04980cd07999b579c52823fc6e3c
MonikaAkther <akthermonika41@gmail.com>
2025-05-31 12:11:34 +0600

    Add files via upload

---
+++ 01/app.js
@@ -0,0 +1,32 @@
+// alert("monika is a full stack web developer");
+// let a=40;
+// let b= 30;
+// console.log(`total $(a+b)`);
+// // console.log(a+b);
+
+
+// let ab=null;
+// console.log(ab);
+// console.log(Number.MAX_SAFE_INTEGER);
+// let name;
+
+function myLearning() {
+    console.log("Monika");
+    console.log("Frontend web development");
+    console.log("PHP web development");
+}
+
+myLearning();
+
+myLearning();
+ 
+
+// let myName="monika";
+
+
+function fullName(){
+    let myName="tahmina";
+    console.log(myName);
+}
+
+fullName()
---
+++ 01/cit.js
@@ -0,0 +1,75 @@
+// alert("monika is a full stack developer")
+
+// function sum(num_1,num_2 =200){
+//     let a=num_1;
+//     let b=num_2;
+//     let sum=a+b;
+//     return sum;
+// }
+
+// console.log(sum(30,))
+// sum(30,20)
+
+
+
+
+
+
+// 2
+
+// let userValueOne=Number(prompt("Enter a vaild Number 1:"));
+// let userValueTwo=Number(prompt("Enter a vaild Number 2:"));
+
+
+
+// function sum(a,b){
+//     let num=a;
+//     let num_2=b;
+//     let sum=a+b;
+//     console.log(`total result is= ${sum}`)
+
+// }
+
+// sum(userValueOne,userValueTwo)
+
+
+
+// let userValueOne=Number(prompt("Enter a vaild Number 1:"));
+// let userValueTwo=Number(prompt("Enter a vaild Number 2:"));
+// let heading= document.querySelector("span");
+
+
+// function sum(a,b){
+//     let num=a;
+//     let num_2=b;
+//     let sum=a+b;
+//     // console.log(`total result is= ${sum}`);
+//     heading.innerHTML= `${sum}`;
+
+
+// }
+
+// sum(userValueOne,userValueTwo)
+
+
+
+let userValueOne= Number(prompt("Enter USD $doller: "));
+let heading= document.querySelector("span")
+function multiply(a,b){
+    // let a=usd;
+    // let b=124;
+    let multiply=a*b;
+    // console.log(multipy)
+    heading.innerHTML= `${multiply}`;
+}
+
+multiply(userValueOne,124);
+// multiply(userValueOne,200);
+
+
+// document.getElementById("demo");
+// let text= document.getElementById("demo").innerHTML= "<h1>hey monika:?</h1>"
+// console.log(text);
+
+// let text2 = document.getElementById("demo").innerText="Full stack web developer";
+// console.log(text2)
